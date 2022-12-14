8f0f45907ca6296ab865b1aa70f8685aa0f60adb
Donald Doricent <ddoricent97@gmail.com>
2022-12-13 20:03:27 -0500

    did exercises for js fundamentals part 3.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function favoriteAnimal(Animal) {
+/* function favoriteAnimal(Animal) {
     return Animal + " Is my favorite animal"
 }
 
-console.log(favoriteAnimal('Goose'))
+console.log(favoriteAnimal('Goose')) */
+  // Commented out for further practice
